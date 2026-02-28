7b5c6fe72f9971ec767e27e05eebaa410eaaa89e
Dragos Chisca <dragoschisca@gmail.com>
2026-02-28 17:50:45 +0200

    ADD: Enums for user and post status

---
+++ src/main/java/com/example/demo/Domain/Enums/PostStatusEnum.java
@@ -0,0 +1,6 @@
+package com.example.demo.Domain.Enums;
+
+public enum PostStatusEnum {
+    DRAFT,
+    PUBLISHED
+}
---
+++ src/main/java/com/example/demo/Domain/Enums/UserStatusEnum.java
@@ -0,0 +1,7 @@
+package com.example.demo.Domain.Enums;
+
+public enum UserStatusEnum {
+    ACTIVE,
+    INACTIVE,
+    BLOCKED
+}
